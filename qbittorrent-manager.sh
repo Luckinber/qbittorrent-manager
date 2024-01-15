670719ec41ce2e9c169ba01981ee0132950b4e96
Lucas Berry <luckinber@gmail.com>
2024-01-15 19:55:55 +1100

    Removed for loop

--- qbittorrent-manager.sh
+++ qbittorrent-manager.sh
@@ -93,9 +93,7 @@ hashes_to_delete=$(printf "|%s" "${hashes_to_delete[@]}")
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     # Delete torrents
     curl -s --data "hashes=${hashes_to_delete:1}&deleteFiles=false" -X POST "$HOST/api/v2/torrents/delete" --cookie "$cookie"
-	for index in ${!torrents_to_delete[@]}; do
-		# Delete directory
-		rm -rf ${directories_to_delete[$index]}
-	done
+	# Delete empty directories
+    rm -r "${directories_to_delete[@]}"
     echo -e "${INFO}Done${NC}"
 fi
